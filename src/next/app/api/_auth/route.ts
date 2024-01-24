545335496f34febfa4365335378dae89c435d064
Jeremy Prowse <jeremyprowse@Jeremys-MacBook-Pro.local>
2024-01-24 18:25:29 +1300

    save changes to auth route

--- src/next/app/api/_auth/route.ts
+++ src/next/app/api/_auth/route.ts
@@ -1,24 +1,21 @@
-// import { redirect } from 'next/navigation'
-// import { cookies } from 'next/headers'
-// import type { NextRequest } from 'next/server'
-// import { NextResponse } from 'next/server'
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
-// export async function GET(request: NextRequest): Promise<Response> {
+export async function GET(request: NextRequest): Promise<Response> {
 
-//   console.log('headers', request.headers)
-//   const authHeader = request.headers.get('authorization')
-//   const token = authHeader?.replace('JWT ', '')
+  const authHeader = request.headers.get('authorization')
+  const token = authHeader?.replace('JWT ', '')
 
-//   console.log('token', token)
+  if (token) {
+    cookies().set('payload-token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 60 * 60 * 24 * 7, // One week
+      path: '/',
+    })
+  }
 
-//   if (token) {
-//     cookies().set('payload-token', token, {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === 'production',
-//       maxAge: 60 * 60 * 24 * 7, // One week
-//       path: '/',
-//     })
-//   }
-
-//   return NextResponse.json({ msg: 'Token received.' }, { status: 200 })
-// }
+  return NextResponse.json({ msg: 'Token received.' }, { status: 200 })
+}
